28c958ad550f2f2b8b2c6ce0d22f435c7f3509ef
Augusto Noriega <augustoxd2343@gmail.com>
2024-12-12 10:32:31 -0300

    Subiendo  Version 2.0

--- main.js
+++ main.js
@@ -1,70 +1,104 @@
-//Inicialización de variables
-let descuento=0;
-let compra=0;
-let total=0;
+//Arrays de productos
+const productos = [
+    {
+        nombre:"ramdd4",
+        precio:1000,
+        stock:2
+    },
+    {
+        nombre:"ryzen5",
+        precio:1600,
+        stock:3
+    },
+    {
+        nombre:"intel9",
+         precio:2000,
+         stock:1
+    },
+    {
+    nombre:"ramdd5",
+    precio:1300,
+    stock:2
+    }
+]
+//Definicion de variables
+let total=0
+let subTotal=0;
+let cantidadProductos=0;
+let productosCompr=[];
+let salir=0;
 
-//definición de funciones
-const suma= (a,b)=> a + b;
-const resta= (a,b)=>a - b;
-const calcIva= a=>a * 0.21;
-function preguntar(){
-    let respuesta=prompt("¿Que desea comprar? placa base 1000$, RAM 200$, CPU 990$ o para salir escriba salir ").toLowerCase();
-    console.log(respuesta);
-    switch(respuesta){
-       case "ram":
-        compra= 200;
-        total=suma(compra,total);
-        console.log(`Por el momento el total a pagar es ${total}`)
-        verificarCont()
-        break;
-       case "cpu":
-        compra= 990;
-        total=suma(compra,total);
-        console.log(`Por el momento el total a pagar es ${total}`)
-        verificarCont()
-        break
-       case "placa base":
-        compra= 1000;
-        total=suma(compra,total);
-        console.log(`Por el momento el total a pagar es ${total}`)
-        verificarCont()
-        break;
-       case "salir":
-        descuento=calcIva(total)+(total*0.1)
-        total=total-descuento;
-        console.log(`Por el momento el total a pagar es ${total}`)
-        return alert("Esperamos verlo pronto");
-        break ;
-       default:
-        alert("No se le comprendió.");
-        return preguntar();
-     }
+//definición de funciones 
+const iva = (valor) => valor*0.21;
+
+const inicio = () =>{
+
+    if (confirm("¿Desea filtrar los articulos por precio?")){   
+        return buscarPrecio();
+    }
+    else{
+        productos.forEach(productos => console.log(`Nombre: ${productos.nombre} precio: ${productos.precio}$`))
+        return comprar();
+    }
 }
-function verificarCont(){
-    let respuesta=prompt("¿Desea continuar? si, no o pagar:").toLowerCase();
-    switch(respuesta){
-        case "si":
-            return preguntar();
-            break;
-        case "no":
-            alert("Esperamos verlo pronto");
-            break;
-        case "pagar":
-            descuento=calcIva(total)+(total*0.1);
-            total=total-descuento;
-            alert("El total a pagar es de "+total+"$");
-            return alert("Gracias por su compra.");
-            break;
-        default:
-            console.log("No se le entendio");
-            return verificarCont();
+const buscarPrecio= () =>{
+    let precioMax=Number(prompt( "Ingrese su tope de precio"));
+    let productosEncontrados= productos.filter((element) => element.precio <= precioMax && element.stock>0);
+    let largoArray=productosEncontrados.length;
+    console.log(largoArray);
+    
+    if(largoArray < 1){
+        alert(`No hemos encontrado ningun producto debajo de los ${precioMax}$ `)
+        let confirmacion=prompt("Escriba salir si no desea comprar nada, escriba filtrar para volver a filtrar o escriba mostrar para mostrar la lista de productos desordenada")
+        switch(confirmacion){
+            case "salir":
+                alert("Esperamos verlo pronto")
+                salir=1
+                break ;
+
         }
+    }
+    else{
+        productosEncontrados.forEach(element => console.log(`Hemos encontrado estos productos: ${element.nombre} Precio: ${element.precio} `));
+        if (confirm("¿Desea comprar alguno de estos productos?")){
+
+            return comprar();
+        }
+        else{
+            return inicio();
+        };
+    }
 }
 
+const comprar = () =>{
+    let productoAComprar=prompt("¿Que producto desea comprar?:").toLowerCase();
+    let indice= productos.findIndex((element) => element.nombre === productoAComprar);
+    console.log(indice);
+    if (indice !=-1){
+        productos[indice].stock-=1
+        alert(`Producto agregado al carrito :Nombre: ${productos[indice].nombre} Precio:${productos[indice].precio}$`);
+        subTotal+=productos[indice].precio
+        cantidadProductos+=1
+        productosCompr.push(productos[indice].nombre)
+        console.log(`Productos:${productosCompr} Total:${subTotal} cantidad de productos adquiridos :${cantidadProductos}`)
+        if(confirm("Desea continuar comprando?")){
+            return inicio();
+        }
+        else{
 
+            console.log(iva(subTotal))
 
-//Ejecución
-alert("Bienvenido a PC componentes estamos en 10% de descuento.");
-alert("Para poner en el carrito el producto que desea comprar escriba el nombre del mismo.");
-alert("Se le va a descontar el 21% de IVA por la ley 19640");
-preguntar();
+             total=subTotal + iva(subTotal)
+            alert(`El total a pagar son ${total}\n Productos adquiridos:${productosCompr}`)
+            return alert("Muchas gracias por su compra esperamos verlo pronto.")
+        }
+
+    }
+    else{
+        alert("Disculpe no tenemos ese producto en stock")
+        return comprar();
+    }
+
+}
+alert("Bienvenido a PComponentes")
+inicio();
